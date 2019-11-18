4ce774bb6adff0f380b47c3a927dd2fa9ba2a62b
lilraisin <olehsander.karonski@gmail.com>
2019-11-19 00:06:07 +0100

    Dokończenie
    Brakuje dosłownie dwóch jakiś pomniejszych propertiesów

---
+++ sandbox.js
@@ -0,0 +1,89 @@
+var currentNode;
+var idcount = 0;
+
+function start() {
+    document.getElementById("selectButton").addEventListener(
+        "click", select, false);
+    document.getElementById("insertButton").addEventListener(
+        "click", insert, false);
+    document.getElementById("appendButton").addEventListener(
+        "click", appendNode, false);
+    document.getElementById("replaceButton").addEventListener(
+        "click", replaceCurrent, false);
+    document.getElementById("removeButton").addEventListener(
+        "click", remove, false);
+    document.getElementById("backgroundColorButton").addEventListener(
+        "click", changeBackgroundColor, false);
+    document.getElementById("textColorButton").addEventListener(
+        "click", changeTextColor, false);
+    document.getElementById("fontlist").addEventListener(
+        "change", changeFont, false);
+    currentNode = document.getElementById("index1");
+}
+
+window.addEventListener("load", start, false);
+
+function highlight(node) {
+    currentNode.setAttribute("class", "");
+    currentNode = node;
+    currentNode.setAttribute("class", "highlight");
+    document.getElementById("gbi").value = currentNode.getAttribute("id");
+}
+
+function select() {
+    var id = document.getElementById("gbi").value;
+    var target = document.getElementById(id);
+    if (target) {
+        highlight(target);
+    }
+}
+
+function insert() {
+    var text = document.getElementById("ins").value;
+    var newNode = document.createTextNode("[new" + idcount + "]" + text);
+    currentNode.parentNode.insertBefore(newNode, currentNode);
+    newNode.setAttribute("id", "new" + idcount++);
+}
+
+function appendNode() {
+    var text = document.getElementById("append").value;
+    var textNode = document.createTextNode("[new" + idcount + "]" + text);
+    currentNode.appendChild(textNode);
+    newNode.setAttribute("id", "new" + idcount++);
+}
+
+function replaceCurrent() {
+    var text = document.getElementById("replace").value;
+    var newNode = createNode("[new" + idcount + "] " + text);
+    currentNode.parentNode.replaceChild(newNode, currentNode);
+    highlight(newNode);
+}
+
+function remove() {
+    newNode = currentNode.parentNode;
+    currentNode.parentNode.removeChild(currentNode);
+    highlight(newNode);
+}
+
+function changeBackgroundColor() {
+    var color = document.getElementById("color").value;
+    currentNode.style.background = color;
+    //currentNode.setAttribute("style", "background-color: " + color)       // można to zrobic tak, ale usunie to inne przywary stylu
+}
+
+function changeTextColor() {
+    var color = document.getElementById("textColor").value;
+    currentNode.style.color = color;
+}
+
+function changeFont() {
+    var font = document.getElementById("fontlist").value;
+    currentNode.setAttribute("style", "font-family: " + font);
+}
+
+function createNode(text) {
+    var newNode = document.createElement("p");
+    newNode.setAttribute("id", "new" + idcount++);
+    newNode.appendChild(document.createTextNode(text));
+    return newNode;
+}
--- thanks.js
+++ thanks.js
@@ -1,49 +1,69 @@
+var xcordmouse;
+var ycordmouse;
+var xcord;
+var ycord;
 
-function createCanvas()
-{
-   var side = 50;
-   var tbody = document.getElementById( "tablebody" );
-  
-   for ( var i = 0; i < side; ++i )
-   {
-      var row = document.createElement( "tr" );
-       
-      for ( var j = 0; j < side; ++j )
-      {
-         var cell = document.createElement( "td" );
-         row.appendChild( cell );
-      } // end for
+function createCanvas() {
+    var side = 50;
+    var tbody = document.getElementById("tablebody");
 
-      tbody.appendChild( row );
-   } // end for
 
-   // register mousemove listener for the table
-   document.getElementById( "canvas" ).addEventListener( 
-      "mousemove", processMouseMove, false );
-} // end function createCanvas
+    for (var i = 0; i < side; ++i) {
+        var row = document.createElement("tr");
 
-// processes the onmousemove event
-function processMouseMove( e )
-{        
-   if ( e.target.tagName.toLowerCase() == "td" )
-   {
-      // turn the cell blue if the Ctrl key is pressed
-      if ( e.ctrlKey )
-      {
-         e.target.setAttribute( "class", "blue" );
-      } // end if
+        for (var j = 0; j < side; ++j) {
+            var cell = document.createElement("td");
+            row.appendChild(cell);
+        }
 
-      // turn the cell red if the Shift key is pressed
-      if ( e.shiftKey )
-      {
-         e.target.setAttribute( "class", "red" );
-      }
+        tbody.appendChild(row);
+    }
 
-		if ( e.altKey )
-      {
-         e.target.setAttribute( "class", "yellow" );
-      } // end if
-   } // end if
-} // end function processMouseMove
+    document.getElementById("canvas").addEventListener(
+        "mousemove", processMouseMove, false);
+    document.getElementById("canvas").addEventListener(
+        "mousedown", processMouseDown, false);
+    document.addEventListener(
+        "mousemove", processCoordinates, false);
+    document.addEventListener(
+        "keydown", keyPressed, false);
+}
 
-window.addEventListener( "load", createCanvas, false );
+function processMouseMove(e) {
+    if (e.target.tagName.toLowerCase() == "td") {
+        if (e.ctrlKey) {
+            e.target.setAttribute("class", "blue");
+        }
+
+        if (e.shiftKey) {
+            e.target.setAttribute("class", "red");
+        }
+
+        if (e.altKey) {
+            e.target.setAttribute("class", "yellow");
+        }
+    }
+
+}
+
+function processMouseDown(e) {
+    if (e.target.tagName.toLowerCase() == "td") {
+        e.target.setAttribute("class", "black");
+
+    }
+}
+
+function processCoordinates(e) {
+    xcordmouse = e.clientX;
+    ycordmouse = e.clientY;
+    xcord = e.screenX;
+    ycord = e.screenY;
+}
+
+function keyPressed(e) {
+    if (e.keyCode === 17) {
+        document.getElementById("mousecoords").innerHTML = "client coords: " + xcordmouse + ", " + ycordmouse;
+        document.getElementById("coords").innerHTML = "screen coords: " + xcordmouse + ", " + ycordmouse;
+    }
+}
+window.addEventListener("load", createCanvas, false);
